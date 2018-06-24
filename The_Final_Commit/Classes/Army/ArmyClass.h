d6f1e30d095ff7650fc325abc195cbad142b6978
shizi991016 <38161767+shizi991016@users.noreply.github.com>
2018-06-24 23:58:54 +0800

    Add files via upload

---
+++ The_Final_Commit/Classes/Army/ArmyClass.h
@@ -0,0 +1,61 @@
+#ifndef ArmyClass_h
+#define ArmyClass_h
+
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+using namespace ui;
+
+
+class BuildingsClass : public cocos2d::Sprite
+{
+public:
+    void setLifeValue(int LifeValue)
+    {
+        _LifeValue = LifeValue;
+    }
+    
+    int getLifeValue()
+    {
+        return _LifeValue;
+    }
+    
+    void setHP(LoadingBar* HP)
+    {
+        _HP = HP;
+    }
+    
+    LoadingBar* getHP()
+    {
+        return _HP;
+    }
+    
+    void setHPInterval(float HPInterval)
+    {
+        _HPInterval = HPInterval;
+    }
+    
+    float getHPInterval()
+    {
+        return _HPInterval;
+    }
+    
+    static BuildingsClass* createWithSpriteFileName(const std::string& FileName)
+    {
+        BuildingsClass* Building = new BuildingsClass();
+        if (Building && Building->initWithFile(FileName))
+        {
+            Building->autorelease();
+            return Building;
+        }
+        CC_SAFE_DELETE(Building);
+        return nullptr;
+    }
+private:
+    int _LifeValue = 100;
+    LoadingBar* _HP;
+    float _HPInterval;
+};
+
+#endif /* ArmyClass_h */
---
+++ The_Final_Commit/Classes/Army/BaseAndFlags.h
@@ -0,0 +1,64 @@
+#ifndef BaseAndFlags_h
+#define BaseAndFlags_h
+
+#include "ArmyClass.h"
+
+constexpr int MapSize = 1601;
+
+class BaseAndFlags
+{
+public:
+    //char IsPositionHaveBuildings[MapSize][MapSize] = {0};
+    //char IsPositionHaveMiningYard[MapSize][MapSize] = {0};
+    
+    int MyEdgeSelection;
+    
+    int MyCountryChoice = 0;
+    int MyClientChoice = 0;
+    
+    std::string MyChat;
+    
+    int ElectricPowerPlantNumber = 0;
+    int RefineryNumber = 0;
+    int BarracksNumber = 0;
+    int WarFactoryNumber = 0;
+    
+    int MyMoney = 100;
+    
+    cocos2d::Point TheLastBarracksPosition;
+    cocos2d::Point TheLastWarFactoryPosition;
+    
+    cocos2d::Point LastTouchPosition;
+    bool IsTouchPositionAvailable = 0;
+    
+    //std::vector<BuildingsClass*> MyBuildings;//用于在update函数中遍历数组，HP<=0删除
+    std::vector<BuildingsClass*> MyElectricPowerPlant;
+    std::vector<BuildingsClass*> MyRefinery;
+    std::vector<BuildingsClass*> EmenyRefinery;
+    std::vector<BuildingsClass*> MyBarracks;
+    std::vector<BuildingsClass*> MyWarFactory;
+    std::vector<BuildingsClass*> MyMissileWell;
+    
+    std::vector<BuildingsClass*> MyTank;
+    std::vector<BuildingsClass*> MyBattlePlane;
+    std::vector<BuildingsClass*> MySoldier;
+    std::vector<BuildingsClass*> MyJet;
+    std::vector<BuildingsClass*> MyTTank;
+    std::vector<BuildingsClass*> MyRTank;
+    std::vector<BuildingsClass*> MySoldierX;
+    std::vector<BuildingsClass*> MyCannon;
+    
+    Point ArmyFirstTouchPosition;
+    Point ArmyLastTouchPosition;
+    
+    int TagNumber = 0;
+    //int PositionTag[MapSize][MapSize] ={0};
+    //int IsPositionHaveArmyAndTag[MapSize][MapSize] = {0};
+    
+    BuildingsClass* MyBase;
+    
+};
+
+
+
+#endif /* BaseAndFlags_h */
